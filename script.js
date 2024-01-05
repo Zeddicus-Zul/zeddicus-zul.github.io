5591575c62f9ac686c44160615b8bee6b7574cc1
Jordan Bailey <jordan.bailey2@yahoo.com>
2024-01-05 09:49:22 -0800

    Adding New JS File

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// script.js
+
 function submitComment() {
-    
+    var name = document.getElementById('name').value;
+    var comment = document.getElementById('comment').value;
+
+    if (name && comment) {
+        var commentsDiv = document.getElementById('comments');
+        var newComment = document.createElement('div');
+        newComment.innerHTML = '<strong>' + name + ':</strong> ' + comment + '<br>';
+
+        commentsDiv.appendChild(newComment);
+
+        // Clear the form fields after submission
+        document.getElementById('name').value = '';
+        document.getElementById('comment').value = '';
+    } else {
+        alert('Please enter both name and comment before submitting.');
+    }
 }
